4082a6290d01c32bd982dc31a87cbe1cdfd8cf5d
Diesel-19 <aselimulgur@gmail.com>
2020-08-04 21:01:03 +0200

    added chapter04 lottery

--- chapter03/Exercise03_29.java
+++ chapter03/Exercise03_29.java
@@ -1,5 +1,8 @@
 package chapter03;
 
 public class Exercise03_29 {
+	public static void main(String[] args) {
+		System.exit(0);
+	}
 
 }
---
+++ chapter04/Chapter04_Lottery.java
@@ -0,0 +1,32 @@
+package chapter04;
+
+import java.util.Scanner;
+
+public class Chapter04_Lottery {
+	public static void main(String[] args) {
+		String lottery = "" + (int) (Math.random() * 9 + 1) + (int) (Math.random() * 10);
+		
+		Scanner input = new Scanner(System.in);
+		System.out.print("Guess a two digit number ");
+		String guess = input.nextLine();
+		
+		char guessDigit1 = guess.charAt(0);
+		char guessDigit2 = guess.charAt(1);
+		
+		char lotteryDigit1 = lottery.charAt(0);
+		char lotteryDigit2 = lottery.charAt(1);
+		
+		
+		if (guess == lottery) {
+			System.out.println("You won €10.000");
+		} else if ((guessDigit1 == lotteryDigit2 && guessDigit2 == lotteryDigit1)) {
+			System.out.println("You won €3.000");
+		} else if (guessDigit1 == lotteryDigit1 || guessDigit1 == lotteryDigit2 || guessDigit2 ==lotteryDigit1 || guessDigit2 == lotteryDigit2) {
+			System.out.println("You won €1.000");
+		} else {
+			System.out.println("You won nothing");
+		}
+		System.out.println("The lottery number was " + lottery);
+	}
+
+}
