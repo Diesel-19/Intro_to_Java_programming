054593779da3a614b92bcfe518b1b31441d0d9e4
Diesel-19 <aselimulgur@gmail.com>
2020-08-05 18:27:09 +0200

    completed chapter04

--- chapter04/Exercise04_06.java
+++ chapter04/Exercise04_06.java
@@ -2,8 +2,30 @@ package chapter04;
 
 public class Exercise04_06 {
 	public static void main(String[] args) {
-		double a = Math.toRadians(Math.random() * 360);
-		
-		
+		double n = Math.toRadians(Math.random() * 360);
+		final double RADIUS = 40;
+
+		double x1 = RADIUS * Math.cos(n);
+		double y1 = RADIUS * Math.sin(n);
+
+		n = Math.toRadians(Math.random() * 360);
+
+		double x2 = RADIUS * Math.cos(n);
+		double y2 = RADIUS * Math.sin(n);
+
+		n = Math.toRadians(Math.random() * 360);
+
+		double x3 = RADIUS * Math.cos(n);
+		double y3 = RADIUS * Math.sin(n);
+
+		double d12 = Math.pow((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2), 0.5);
+		double d23 = Math.pow((x3 - x2) * (x3 - x2) + (y3 - y2) * (y3 - y2), 0.5);
+		double d31 = Math.pow((x1 - x3) * (x1 - x3) + (y1 - y3) * (y1 - y3), 0.5);
+
+		double a = Math.toDegrees(Math.acos((d23 * d23 - d31 * d31 - d12 * d12) / (-2 * d31 * d12)));
+		double b = Math.toDegrees(Math.acos((d31 * d31 - d23 * d23 - d12 * d12) / (-2 * d23 * d12)));
+		double c = Math.toDegrees(Math.acos((d12 * d12 - d31 * d31 - d23 * d23) / (-2 * d23 * d31)));
+
+		System.out.println(a + " " + b + " " + c);
 	}
 }
--- chapter04/Exercise04_07.java
+++ chapter04/Exercise04_07.java
@@ -5,11 +5,26 @@ import java.util.Scanner;
 public class Exercise04_07 {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
-		
+
 		System.out.println("Enter the radius of the bounding circle: ");
 		double radius = input.nextDouble();
 		
-		
+		double x1 = radius * Math.cos(Math.toRadians(18));
+		double y1 = radius * Math.sin(Math.toRadians(18));
+		double x2 = radius * Math.cos(Math.toRadians(90));
+		double y2 = radius * Math.sin(Math.toRadians(90));
+		double x3 = radius * Math.cos(Math.toRadians(162));
+		double y3 = radius * Math.sin(Math.toRadians(162));
+		double x4 = radius * Math.cos(Math.toRadians(234));
+		double y4 = radius * Math.sin(Math.toRadians(234));
+		double x5 = radius * Math.cos(Math.toRadians(306));
+		double y5 = radius * Math.sin(Math.toRadians(306));
+
+		System.out.println("(" + x1 + ", " + y1 + ")");
+		System.out.println("(" + x2 + ", " + y2 + ")");
+		System.out.println("(" + x3 + ", " + y3 + ")");
+		System.out.println("(" + x4 + ", " + y4 + ")");
+		System.out.println("(" + x5 + ", " + y5 + ")");
 	}
 
 }
