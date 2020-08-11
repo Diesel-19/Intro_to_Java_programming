668e1f081f631bb3d3ab3fbd24e35dd79d868e77
Diesel-19 <aselimulgur@gmail.com>
2020-08-11 22:47:17 +0200

    added skipped chapter05 exercises

--- chapter05/Exercise05_22.java
+++ chapter05/Exercise05_22.java
@@ -1,9 +1,35 @@
 package chapter05;
 
+import java.util.Scanner;
+
 public class Exercise05_22 {
 	public static void main(String[] args) {
-		
-		
+		Scanner input = new Scanner(System.in);
+
+		System.out.println("Enter loan amount: ");
+		double loanAmount = input.nextDouble();
+
+		System.out.println("Number of years: ");
+		int years = input.nextInt();
+
+		System.out.println("Annual interest rate: ");
+		double annualInterestRate = input.nextDouble();
+
+		double monthlyInterestRate = annualInterestRate / 1200;
+		double monthlyPayment = loanAmount * monthlyInterestRate
+				/ (1 - 1 / Math.pow(1 + monthlyInterestRate, years * 12));
+		System.out.printf("Monthly Payment: %.2f\n", monthlyPayment);
+		System.out.printf("Total Payment: %.2f\n", (monthlyPayment * 12) * years);
+
+		double balance = loanAmount, principal, interest;
+		System.out.println("Payment#     Interest     Principal     Balance");
+		for (int i = 1; i <= years * 12; i++) {
+			interest = monthlyInterestRate * balance;
+			principal = monthlyPayment - interest;
+			balance = balance - principal;
+			System.out.printf("%-13d%-13.2f%-13.2f%.2f\n", i, interest, principal, balance);
+		}
+
 	}
 
 }
--- chapter05/Exercise05_29.java
+++ chapter05/Exercise05_29.java
@@ -1,7 +1,120 @@
 package chapter05;
 
+import java.util.Scanner;
+
 public class Exercise05_29 {
 	public static void main(String[] args) {
-		
+		Scanner input = new Scanner(System.in);
+
+		System.out.println("Enter year: (e.g., 2012): ");
+		int year = input.nextInt();
+		System.out.println("Enter first day of the year: ");
+		int day = input.nextInt();
+
+		String output;
+		for (int month = 1; month <= 12; month++) {
+			output = "";
+			switch (month) {
+			case 1:
+				output += "January ";
+				break;
+			case 2:
+				output += "February ";
+				break;
+			case 3:
+				output += "March ";
+				break;
+			case 4:
+				output += "April ";
+				break;
+			case 5:
+				output += "May ";
+				break;
+			case 6:
+				output += "June ";
+				break;
+			case 7:
+				output += "July ";
+				break;
+			case 8:
+				output += "August ";
+				break;
+			case 9:
+				output += "September ";
+				break;
+			case 10:
+				output += "October ";
+				break;
+			case 11:
+				output += "November ";
+				break;
+			case 12:
+				output += "December ";
+				break;
+			}
+			output += " " + year;
+			System.out.println(output);
+			System.out.println("-------------------------------------");
+			System.out.println("Sun  Mon  Tue  Wed  Thu  Fri  Sat");
+
+			switch (day % 7) {
+			case 0:
+
+				break;
+			case 1:
+				System.out.print("     ");
+				break;
+			case 2:
+				System.out.print("          ");
+				break;
+			case 3:
+				System.out.print("               ");
+				break;
+			case 4:
+				System.out.print("                    ");
+				break;
+			case 5:
+				System.out.print("                         ");
+				break;
+			case 6:
+				System.out.print("                              ");
+				break;
+			}
+			
+			int dayInMonth;
+			if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12) {
+				dayInMonth = 31;
+			} else if (month == 4 || month == 6 || month == 9 || month == 11) {
+				dayInMonth = 30;
+			} else {
+				if (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0)) {
+					dayInMonth = 29;
+				} else {
+					dayInMonth = 28;
+				}
+			}
+			for (int i = 1; i <= dayInMonth; i++) {
+				System.out.printf("%3d  ", i);
+				if ((i + day % 7) % 7 == 0) {
+					System.out.println();
+				}
+			}
+			System.out.println();
+			System.out.println();
+
+			day %= 7;
+			if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12) {
+				day += 31;
+			} else if (month == 4 || month == 6 || month == 9 || month == 11) {
+				day += 30;
+			} else {
+				if (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0)) {
+					day += 29;
+				} else {
+					day += 28;
+				}
+			}
+		}
+
 	}
 }
