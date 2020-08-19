118249f202508eff4fc85ae6696b0177bf1b5efe
Diesel-19 <aselimulgur@gmail.com>
2020-08-19 06:16:44 +0200

    modified exercise16

--- chapter06/Exercise06_16.java
+++ chapter06/Exercise06_16.java
@@ -2,18 +2,61 @@ package chapter06;
 
 public class Exercise06_16 {
 	public static void main(String[] args) {
-		for (int i = 2000; i <= 2020; i++) {
-			System.out.println(numberOfDaysInAYear(i));
+		int[][] m = { { 4, 2 }, { 1, 7 }, { 4, 5 }, { 1, 2 }, { 1, 1 }, { 4, 1 } };
+		sort(m);
+		for (int i = 0; i < m.length; i++) {
+			for (int j = 0; j < m[0].length; j++) {
+				System.out.print(m[i][j] + " ");
+			}
+			System.out.println();
 		}
 	}
 
-	public static int numberOfDaysInAYear(int year) {
-		int days = 31 * 7 + 30 * 4;
-		if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
-			days += 29;
-		} else {
-			days += 28;
+	public static void sort(int m[][]) {
+		int[] temp = new int[m[0].length];
+		// sort first column
+		for (int i = 0; i < m.length - 1; i++) {
+			int min = m[i][0];
+			int minIndex = i;
+			for (int j = i + 1; j < m.length; j++) {
+				if (m[j][0] < min) {
+					minIndex = j;
+					min = m[j][0];
+				}
+			}
+			if (i != minIndex) {
+				for (int j2 = 0; j2 < temp.length; j2++) {
+					temp[j2] = m[minIndex][j2];
+				}
+				for (int j2 = 0; j2 < temp.length; j2++) {
+					m[minIndex][j2] = m[i][j2];
+				}
+				for (int j2 = 0; j2 < temp.length; j2++) {
+					m[i][j2] = temp[j2];
+				}
+			}
 		}
-		return days;
+		// sort second column while the first column is still sorted
+		for (int i = 0; i < m.length - 1; i++) {
+			int min = m[i][1];
+			int minIndex = i;
+			for (int j = i + 1; j < m.length; j++) {
+				if (m[j][1] < min && m[i][0] == m[j][0]) {
+					minIndex = j;
+					min = m[j][1];
+				}
+			}
+			if (i != minIndex) {
+				for (int j2 = 0; j2 < temp.length; j2++) {
+					temp[j2] = m[minIndex][j2];
+				}
+				for (int j2 = 0; j2 < temp.length; j2++) {
+					m[minIndex][j2] = m[i][j2];
+				}
+				for (int j2 = 0; j2 < temp.length; j2++) {
+					m[i][j2] = temp[j2];
+				}
+			}
+		}
 	}
 }
