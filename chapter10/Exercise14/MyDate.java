422f0b2a8967f8076c8696a454cbd6fe79f477ea
Diesel-19 <aselimulgur@gmail.com>
2020-08-28 19:52:01 +0200

    added all chapter10 exercises

--- chapter10/Exercise14/MyDate.java
+++ chapter10/Exercise14/MyDate.java
@@ -1,5 +1,15 @@
 package chapter10.Exercise14;
 
 public class MyDate {
-
+	private int year;
+	private int month;
+	private int day;
+	
+	public MyDate() {
+		
+	}
+	
+	public MyDate (long elapsedTime) {
+		
+	}
 }
