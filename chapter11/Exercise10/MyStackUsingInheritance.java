894ee876a2e165c5b7c252353f49e6be0cd5d9b3
Diesel-19 <aselimulgur@gmail.com>
2020-09-01 19:06:36 +0200

    added skipped chapter11 exercises

--- chapter11/Exercise10/MyStackUsingInheritance.java
+++ chapter11/Exercise10/MyStackUsingInheritance.java
@@ -2,6 +2,24 @@ package chapter11.Exercise10;
 
 import java.util.ArrayList;
 
-public class MyStackUsingInheritance extends ArrayList {
+public class MyStackUsingInheritance extends ArrayList<Object> {
 
+	public Object peek() {
+		return get(size() - 1);
+	}
+
+	public Object pop() {
+		Object o = get(size() - 1);
+		remove(size() - 1);
+		return o;
+	}
+
+	public void push(Object o) {
+		add(o);
+	}
+
+	@Override
+	public String toString() {
+		return "stack: " + toString();
+	}
 }
