483387caf302edd48548cfa76c7ccd1b1bff8652
RohanHarichandan <72160268+RohanHarichandan@users.noreply.github.com>
2020-10-01 20:12:14 +0530

    Update Exercise01_01.java

--- chapter01/Exercise01_01.java
+++ chapter01/Exercise01_01.java
@@ -3,6 +3,7 @@ package chapter01;
 public class Exercise01_01 {
 
 	public static void main(String[] args) {
+		system.out.println("Hello World");		
 		System.out.println("Welcome to Java");
 		System.out.println("Welcome to Computer Science");
 		System.out.println("Programming is fun");
